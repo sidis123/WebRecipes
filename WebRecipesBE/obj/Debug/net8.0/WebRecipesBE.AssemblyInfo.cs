c4938d8333b3ebde280d31cd7aeeac150a4f7673
Tomas Sakalauskas <tomass.sakalauskas@gmail.com>
2024-10-04 09:14:36 +0300

    preparing DB context

--- WebRecipesBE/obj/Debug/net8.0/WebRecipesBE.AssemblyInfo.cs
+++ WebRecipesBE/obj/Debug/net8.0/WebRecipesBE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebRecipesBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4afefca258e846c842f2166988fb5fdc48662bf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dff7f952f6de948de6bb0851f80860012210ad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebRecipesBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebRecipesBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
