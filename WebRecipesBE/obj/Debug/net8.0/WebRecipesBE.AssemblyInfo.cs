6ea85401c96909f0f54f4ba0b52dba112d39d729
Tomas Sakalauskas <tomass.sakalauskas@gmail.com>
2024-10-04 09:38:46 +0300

    injecting DBContext

--- WebRecipesBE/obj/Debug/net8.0/WebRecipesBE.AssemblyInfo.cs
+++ WebRecipesBE/obj/Debug/net8.0/WebRecipesBE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebRecipesBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dff7f952f6de948de6bb0851f80860012210ad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4938d8333b3ebde280d31cd7aeeac150a4f7673")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebRecipesBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebRecipesBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
